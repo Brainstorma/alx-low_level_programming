96d83271b1f364f1e4e3a79cde835376405d47d8
brainstormabaaj <brainstormabaaj@gmail.com>
2022-10-14 15:39:02 -0700

    automated commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints the alphabet in lowercase, except q and e
+ * Challenge: You can only use the putchar twice
+ * Return: 0 if result is as expected otherwise !0
+ */
+
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		if (l != 'e' && l != 'q')
+		{
+			putchar(l);
+		}
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
