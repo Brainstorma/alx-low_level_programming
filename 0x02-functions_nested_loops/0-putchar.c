5298c774ef40c815b0c397bd12fb766b01e81791
brainstormabaaj <brainstormabaaj@gmail.com>
2022-10-18 12:49:48 -0700

    automated commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/*
+/**
  * main - program that prints _putchar, followed by a new line
  * Challenge: No more than 5 functions per file
  * Return: 0 if result is as expected else !0
  */
 
 int main(void)
+
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, ct;
