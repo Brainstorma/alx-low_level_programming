44b56014198999bcc771cf43533feb77c221805e
brainstormabaaj <brainstormabaaj@gmail.com>
2022-10-18 15:01:48 -0700

    automated commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * Challenge: use global variables are prohibited
+ * @fn: int to be added to first input
+ * @sn: int to be added to second input
+ * Return: total
+**/
+
+int add(int fn, int sn)
+{
+	int total;
+
+	sum = fn + sn;
+
+	return (total)
+}
