38b03fd4496967c3f10028d94cd1f69bc149e965
brainstormabaaj <brainstormabaaj@gmail.com>
2022-10-19 14:13:33 -0700

    automated commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Challenge: change only 1 line in this task
+ * Return: 0 if result is as expected else !0
+**/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
