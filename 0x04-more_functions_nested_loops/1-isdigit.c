642c9f03283c57b3cc912966d6098a30cb1991a0
brainstormabaaj <brainstormabaaj@gmail.com>
2022-10-20 15:33:03 -0700

    automated commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a digit (0-9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, else 0
+ **/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first int to be multiplied
+ * @b: second int to be multiplied
+ * Return: multi results
+**/
+
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+
+	return (multi);
+}
