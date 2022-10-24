2db7aa17a83231bca7604dcb6941d91916caf528
brainstormabaaj <brainstormabaaj@gmail.com>
2022-10-24 14:58:55 -0700

    automated commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - a function that prints n elements of an array
+ * Challenge: Numbers should be displayed in the same order they are stored in array.
+ * @a: array name
+ * @n: is the number of elements of the array to be printed
+ * Return: a and n inputs
+ **/
+
+void print_array(int *a, int n)
+{
+	int fn;
+
+	fn = 0;
+	for (n--; n >= 0; n--, fn++)
+	{
+		printf("%d", a[fn]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
