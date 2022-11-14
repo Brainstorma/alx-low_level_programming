370d71d69cfe8fc76e430c09d17ad8a18265d9a8
brainstormabaaj <brainstormabaaj@gmail.com>
2022-11-14 10:23:12 -0800

    automated commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
- * File: 3-function_like_macro.h
- * @x: absolute number
- * main: absolute value of a number x.
- **/
-
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
